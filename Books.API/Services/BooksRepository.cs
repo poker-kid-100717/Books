dcd90fd6ba75bf231cc014395d602c8068ec2f16
Joshua Davis <joshuad100717@outlook.com>
2021-10-08 14:29:51 -0400

    - created api project - skeleton of app - used ef core to use migrations - swagger baked in - created db with 2 tbls - book + author

---
+++ Books.API/Services/BooksRepository.cs
@@ -0,0 +1,52 @@
+﻿using Books.API.Data;
+using Books.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.API.Services
+{
+    public class BooksRepository : IBooksRepository, IDisposable
+    {
+        private ApplicationDbContext _context;
+
+        public BooksRepository(ApplicationDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+              
+
+        public async Task<Book> GetBookAsync(int id)
+        {
+            return await _context.Books
+                .Include(b => b.Author).FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task<IEnumerable<Book>> GetBooksAsync()
+        {
+            return await _context.Books.Include(b => b.Author).ToListAsync();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_context != null)
+                {
+                    _context.Dispose();
+                    _context = null;
+                }
+
+            }
+        }
+
+    }
+}
